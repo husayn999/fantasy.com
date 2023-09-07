3b4d49f102043b167af9a80337dd4692f11ce1a2
husayn999 <139649501+husayn999@users.noreply.github.com>
2023-09-07 14:33:14 +0300

    Add files via upload
    hello world

---
+++ java.js
@@ -0,0 +1,90 @@
+function input() {
+    var name1 = document.getElementById("area") .value;
+    document.getElementById("box-output").innerHTML = name1;
+}
+
+let btn2 = document.getElementById("btn2").value;
+
+
+function input2() {
+    
+    document.getElementById("box-output").innerHTML = btn2;
+ 
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function saymassage(fname, name2) {
+    
+//     let massage = `hello`
+
+//     function concatmassage() {
+        
+//         massage = `${massage} ${fname} ${name2}`;
+//     }
+//     concatmassage();
+//     return massage;
+// }
+// console.log(saymassage("ossama" , "mohamed"));
+
+// function saymassage(fname, name2) {
+    
+//     let massage = `hello`
+
+//     function concatmassage() {
+        
+//        return `${massage} ${fname} ${name2}`;
+//     }
+//     return concatmassage();
+
+// }
+// console.log(saymassage("ossama" , "mohamed"));
+
+// function saymassage(fname, name2) {
+    
+//     let massage = `hello`
+//         function concatmassage() {
+//         function getFullname() {
+//             return `${fname} ${name2}`;
+//         }
+//         return `${massage} ${getFullname()}`;
+//     }
+//     return concatmassage();}
+// console.log(saymassage("ossama" , "mohamed"));
+
+
+// //push unshift pop sort
